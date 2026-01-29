d03b42a950a70305cc431a6f9a48576aad231924
Ashutosh <ashutoshtope139@gmail.com>
2026-01-29 22:16:55 +0530

    add new studentEntity & StudentRepository

---
+++ src/main/java/com/example/demo/Enity/studentEntity.java
@@ -0,0 +1,12 @@
+package com.example.demo.Enity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class studentEntity {
+    Integer id;
+    String name;
+    String surname;
+    String Address;
+
+}
